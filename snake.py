f7a0532db2c29bf3716473f9612928a2cfb8e0a0
IgorGrach0 <igor.grach.00@mail.ru>
2024-05-04 22:50:05 +0500

    first_commit

--- snake.py
+++ snake.py
@@ -14,15 +14,13 @@ black = (0, 0, 0)
 red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
+brown = (201,100,59)
 
 dis_width = 1000
 dis_height = 800
 
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Змейка')
-
-pygame.draw.rect(dis, green, [300, 500, 20, 20])
-pygame.display.update()
 
 clock = pygame.time.Clock()
 
@@ -149,14 +147,29 @@ def gameLoop(before_r_l, before_up_down, snake_speed, This_time, check_update):
             y1 = dis_height
         elif y1 >= dis_height:
             y1 = 0
+
         x1 += x1_change
         y1 += y1_change
         dis.fill(blue)
+
+        coord_rect = [300, 500, 200, 20]
+        pygame.draw.rect(dis, brown, [coord_rect[0], coord_rect[1], coord_rect[2], coord_rect[3]])
+
         pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
         snake_Head = []
         snake_Head.append(x1)
         snake_Head.append(y1)
         snake_List.append(snake_Head)
+        list_cord_game_over_x = []
+        list_cord_game_over_y = []
+        for i in range (int(coord_rect[0]), int(coord_rect[0]) + int(coord_rect[2])):
+            list_cord_game_over_x.append(i)
+
+        for o in range (int(coord_rect[1]), int(coord_rect[1]) + int(coord_rect[3])):
+            list_cord_game_over_y.append(o)
+
+        if x1 in list_cord_game_over_x and y1 in list_cord_game_over_y:
+            game_close = True
         if len(snake_List) > Length_of_snake:
             del snake_List[0]
 
@@ -166,12 +179,10 @@ def gameLoop(before_r_l, before_up_down, snake_speed, This_time, check_update):
 
         our_snake(snake_block, snake_List)
         Your_score(Length_of_snake - 1)
-
-
         pygame.display.update()
 
         list_foodx = []
-        for i in range(51):
+        for i in range(100):
             list_foodx.append(i)
 
         list_foody = []
@@ -180,11 +191,9 @@ def gameLoop(before_r_l, before_up_down, snake_speed, This_time, check_update):
 
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            print(foodx)
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            while foodx in list_foodx and foody in list_foody:
+            while (foodx in list_foodx and foody in list_foody) or (foodx in list_cord_game_over_x and foody in list_cord_game_over_y):
                 foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-                print(foodx)
                 foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
             Length_of_snake += 10
