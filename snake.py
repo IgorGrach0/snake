f89c642641ee52f6cadf529eefa0c5982a5ce9e5
IgorGrach0 <igor.grach.00@mail.ru>
2024-05-08 15:11:39 +0500

    first lvl

--- snake.py
+++ snake.py
@@ -16,6 +16,8 @@ red = (255, 30, 30)
 green = (0, 255, 0)
 blue = (50, 153, 213)
 brown = (201,100,59)
+light = (170, 170, 170)
+dark = (100, 100, 100)
 
 dis_width = 1000
 dis_height = 800
@@ -38,7 +40,8 @@ texture_left = pygame.image.load('face_test_left.png')
 linux_logo = pygame.image.load('Linux_Logo.png')
 
 font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
+score_font = pygame.font.SysFont("comicsansms", 32)
+font_button = pygame.font.SysFont(None, 32)
 
 def record_max_result(Length_of_snake):
     import sqlite3
@@ -69,7 +72,7 @@ def record_max_result(Length_of_snake):
 
 def Your_score(score):
     value = score_font.render("Your Score: " + str(score), True, yellow)
-    dis.blit(value, [10, 0])
+    dis.blit(value, [3, 0])
 
 def Your_level(level):
     value = score_font.render("level: " + str(level), True, yellow)
@@ -102,9 +105,224 @@ def our_snake(snake_block, snake_list, position):
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
-    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+    dis.blit(mesg, [dis_width / 4, dis_height / 3])
+
+
+def first_level(before_r_l, before_up_down, snake_speed, This_time, check_update, pos):
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block - 40) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block - 40) / 10.0) * 10.0
+
+    while not game_over:
+        while game_close == True:
+            if check_update == True:
+                record = record_max_result(Length_of_snake)
+                check_update = False
+                value = score_font.render("Your Record: " + str(record), True, yellow)
+                dis.blit(value, [300, 0])
+                pygame.display.update()
+
+            message("Game over! Press C-Play Again or Q-Quit", red)
+            Your_score(Length_of_snake - 1)
+            
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        first_level(0, 0, 10, time.time(), True, 'up')
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and before_r_l != 1 and time.time() - This_time >= 0.005:
+                    pos = 'left'
+                    before_up_down = 0
+                    x1_change = -snake_block
+                    y1_change = 0
+                    before_r_l = 1
+                    This_time = time.time()
+                elif event.key == pygame.K_RIGHT and before_r_l != 1 and time.time() - This_time >= 0.005:
+                    pos = 'right'
+                    before_up_down = 0
+                    x1_change = snake_block
+                    y1_change = 0
+                    before_r_l = 1
+                    This_time = time.time()
+                elif event.key == pygame.K_UP and before_up_down == 0 and time.time() - This_time >= 0.005:
+                    pos = 'up'
+                    before_up_down = 1
+                    y1_change = -snake_block
+                    x1_change = 0
+                    before_r_l = 0
+                    This_time = time.time()
+                elif event.key == pygame.K_DOWN and before_up_down == 0 and time.time() - This_time >= 0.005:
+                    pos = 'down'
+                    before_up_down = 1
+                    y1_change = snake_block
+                    x1_change = 0
+                    before_r_l = 0
+                    This_time = time.time()
+
+        if x1 >= dis_width + 1:
+            x1 = 0
+        elif x1 < 0:
+            x1 = dis_width
+        elif y1 < 0:
+            y1 = dis_height
+        elif y1 >= dis_height + 1:
+            y1 = 0
+
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)
+
+        # прямоугольники (препятствия и графика)
+        coord_rect = [0, 50, 250, 15]
+        pygame.draw.rect(dis, brown, [coord_rect[0], coord_rect[1], coord_rect[2], coord_rect[3]])
+
+        coord_rect_1 = [250, 0, 15, 65]
+        pygame.draw.rect(dis, brown, [coord_rect_1[0], coord_rect_1[1], coord_rect_1[2], coord_rect_1[3]])
+
+        coord_rect_2 = [450, 300, 250, 250]
+        rect_1 = pygame.draw.rect(dis, blue, [coord_rect_2[0], coord_rect_2[1], coord_rect_2[2], coord_rect_2[3]])
+        for z in range(rect_1.left, rect_1.right, linux_logo.get_width()):
+            for y in range(rect_1.top, rect_1.bottom, linux_logo.get_height()):
+                dis.blit(linux_logo, (z, y))
+
+        coord_rect_7 = [0, 0, 250, 10]
+        pygame.draw.rect(dis, brown, [coord_rect_7[0], coord_rect_7[1], coord_rect_7[2], coord_rect_7[3]])
+
+        coord_rect_8 = [785, 0, 300, 10]
+        pygame.draw.rect(dis, brown, [coord_rect_8[0], coord_rect_8[1], coord_rect_8[2], coord_rect_8[3]])
+
+        coord_rect_9 = [785, 0, 10, 50]
+        pygame.draw.rect(dis, brown, [coord_rect_9[0], coord_rect_9[1], coord_rect_9[2], coord_rect_9[3]])
+
+        coord_rect_10 = [785, 50, 250, 10]
+        pygame.draw.rect(dis, brown, [coord_rect_10[0], coord_rect_10[1], coord_rect_10[2], coord_rect_10[3]])
+
+        coord_rect_11 = [785, 50, 250, 10]
+        pygame.draw.rect(dis, brown, [coord_rect_11[0], coord_rect_11[1], coord_rect_11[2], coord_rect_11[3]])
+
+        pygame.draw.rect(dis, red, [foodx, foody, snake_block, snake_block])
+
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        list_cord_game_over_x = []
+        list_cord_game_over_y = []
+        for i in range(int(coord_rect[0]), int(coord_rect[0]) + int(coord_rect[2])):
+            list_cord_game_over_x.append(i)
+
+        for o in range(int(coord_rect[1]), int(coord_rect[1]) + int(coord_rect[3])):
+            list_cord_game_over_y.append(o)
+
+        # второе препятствие:
+        for i in range(int(coord_rect_1[0]), int(coord_rect_1[0]) + int(coord_rect_1[2])):
+            list_cord_game_over_x.append(i)
+
+        for o in range(int(coord_rect_1[1]), int(coord_rect_1[1]) + int(coord_rect_1[3])):
+            list_cord_game_over_y.append(o)
+
+        if (x1 in list_cord_game_over_x and y1 in list_cord_game_over_y) or(
+                x1 < 250 and y1 < 70) or (x1 > 775 and y1 < 60):
+            game_close = True
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List, pos)
+
+        Your_score(Length_of_snake - 1)
+        Your_level(lvl)
+
+        list_foodx = []
+        for i in range(100):
+            list_foodx.append(i)
+
+        list_foody = []
+        for i in range(51):
+            list_foody.append(i)
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+            snake_speed += 0.5
+
+        while (foodx in list_foodx and foody in list_foody) or (
+                foodx in list_cord_game_over_x and foody in list_cord_game_over_y) or (
+                foodx < 250 and foody < 65) or (foodx > 775 and foody < 60):
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+        pygame.display.update()
+
+        clock.tick(snake_speed)
+    pygame.quit()
+    quit()
+
 def main():
-    
+    # Создайте поверхность для кнопки
+    button_surface = pygame.Surface((150, 50))
+    # Отображение текста на кнопке
+    text = font_button.render("Новая Игра", True, (0, 0, 0))
+    text_rect = text.get_rect(
+        center=(button_surface.get_width() / 2,
+                button_surface.get_height() / 2))
+
+    # Создайте объект pygame.Rect, который представляет границы кнопки
+    button_rect = pygame.Rect(400, 125, 150, 50)  # Отрегулируйте положение
+    while True:
+        clock.tick(60)
+        dis.fill(blue)
+        # Получаем события из очереди событий
+        for event in pygame.event.get():
+            import sys
+            # Проверьте событие выхода
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            # Проверяем событие нажатия кнопки мыши
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # Вызовите функцию on_mouse_button_down()
+                if button_rect.collidepoint(event.pos):
+                    first_level(0, 0, snake_speed_0, 0, True, 'up')
+        # Проверьте, находится ли мышь над кнопкой.
+        # Это создаст эффект наведения кнопки.
+        if button_rect.collidepoint(pygame.mouse.get_pos()):
+            pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
+        else:
+            pygame.draw.rect(button_surface, (0, 0, 0), (0, 0, 150, 50))
+            pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
+            pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, 148, 1), 2)
+            pygame.draw.rect(button_surface, (0, 100, 0), (1, 48, 148, 10), 2)
+        # Показать текст кнопки
+        button_surface.blit(text, text_rect)
+        # Нарисуйте кнопку на экране
+        dis.blit(button_surface, (button_rect.x, button_rect.y))
+        # Обновить состояние
+        pygame.display.update()
 
 def gameLoop(before_r_l, before_up_down, snake_speed, This_time, check_update, pos):
     game_over = False
@@ -128,7 +346,6 @@ def gameLoop(before_r_l, before_up_down, snake_speed, This_time, check_update, p
             if check_update == True:
                 record = record_max_result(Length_of_snake)
                 check_update = False
-
                 value = score_font.render("Your Record: " + str(record), True, yellow)
                 dis.blit(value, [300, 0])
                 pygame.display.update()
@@ -337,5 +554,5 @@ def gameLoop(before_r_l, before_up_down, snake_speed, This_time, check_update, p
         clock.tick(snake_speed)
     pygame.quit()
     quit()
-
-gameLoop(0, 0, snake_speed_0, 0, True, 'up')
+main()
+#gameLoop(0, 0, snake_speed_0, 0, True, 'up')
--- test.py
+++ test.py
@@ -1,49 +1,51 @@
 
 
 
+def button_game_Over():
+    import snake
+    import sys
+    import pygame
 
+    pygame.init()
 
+    clock = pygame.time.Clock()
+    # Создайте поверхность для кнопки
+        button_surface = pygame.Surface((150, 50))
+        # Отображение текста на кнопке
+        text = font_button.render("Новая Игра", True, (0, 0, 0))
+        text_rect = text.get_rect(
+            center=(button_surface.get_width() / 2,
+                    button_surface.get_height() / 2))
 
-
-'''import pygame
-import sys
-from pygame.color import THECOLORS
-
-pygame.init()
-
-screen = pygame.display.set_mode((1000, 800))
-
-screen.fill(THECOLORS['green'])
-font = pygame.font.SysFont('couriernew', 25, True)
-text = font.render(str('Счет:'), True, THECOLORS['white'])
-screen.blit(text, (25, 10))
-is_blue = True
-x = 30
-y = 30
-
-clock = pygame.time.Clock()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        is_blue = not is_blue
-
-    pressed = pygame.key.get_pressed()
-    screen.fill(THECOLORS['green'])
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
-
-    if is_blue:
-        color = (0, 128, 255)
-    else:
-        color = (255, 100, 0)
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
-
-    pygame.display.flip()
-    clock.tick(60)'''
+        # Создайте объект pygame.Rect, который представляет границы кнопки
+        button_rect = pygame.Rect(400, 125, 150, 50)  # Отрегулируйте положение
+        while True:
+            clock.tick(60)
+            dis.fill(color)
+            # Получаем события из очереди событий
+            for event in pygame.event.get():
+                import sys
+                # Проверьте событие выхода
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                # Проверяем событие нажатия кнопки мыши
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    # Вызовите функцию on_mouse_button_down()
+                    if button_rect.collidepoint(event.pos):
+                        snake.first_level(0, 0, snake_speed_0, 0, True, 'up')
+            # Проверьте, находится ли мышь над кнопкой.
+            # Это создаст эффект наведения кнопки.
+            if button_rect.collidepoint(pygame.mouse.get_pos()):
+                pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
+            else:
+                pygame.draw.rect(button_surface, (0, 0, 0), (0, 0, 150, 50))
+                pygame.draw.rect(button_surface, (255, 255, 255), (1, 1, 148, 48))
+                pygame.draw.rect(button_surface, (0, 0, 0), (1, 1, 148, 1), 2)
+                pygame.draw.rect(button_surface, (0, 100, 0), (1, 48, 148, 10), 2)
+            # Показать текст кнопки
+            button_surface.blit(text, text_rect)
+            # Нарисуйте кнопку на экране
+            dis.blit(button_surface, (button_rect.x, button_rect.y))
+            # Обновить состояние
+            pygame.display.update()
